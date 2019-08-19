d1c4f553ad980aba870f18ec17e20af7c32f75e2
Richard Lee <richlee81192@gmail.com>
2019-08-19 19:28:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(string)
-  array = string.split(" ")
-  test_array = []
-  array.collect do|string| 
-    test_array << string.reverse
-  end
-  test_array.join(" ")
+  # def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+# end 
+# end
   end
